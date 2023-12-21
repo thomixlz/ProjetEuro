f076592929279b2e8acd61653c2fd641e72e0593
thomixlz <thomasbregler@protonmail.com>
2023-12-21 11:35:36 +0100

    generate all matches and scores for group stage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\MatchGroup;
 use App\Repository\TeamsRepository;
+use App\Repository\MatchGroupRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
 
 class DefaultController extends AbstractController {
 
@@ -13,8 +19,7 @@ class DefaultController extends AbstractController {
 /**
 * @Route("/", name="app_index")
 */
-public function index(TeamsRepository $teamsRepository): Response
-{
+public function index(TeamsRepository $teamsRepository, MatchGroupRepository $matchGroupRepository): Response {
 
     $teams = $teamsRepository->findAll();
     $teamsByRank = $teamsRepository->findBy([], ['rankFifa' => 'ASC']);
@@ -25,6 +30,15 @@ public function index(TeamsRepository $teamsRepository): Response
     $teamsPOT4 = $teamsRepository->findBy(['Chapeau' => 'POT4']);
 
     $teamsBarrer = $teamsRepository->findBy(['Chapeau' => null]);
+
+    $matches = $matchGroupRepository->findAll();
+
+    // Grouper les matchs par chapeau
+    $matchesByGroup = [];
+    foreach ($matches as $match) {
+        $chapeau = $match->getTeam1()->getChapeau();
+        $matchesByGroup[$chapeau][] = $match;
+    }
 
 
 
@@ -39,20 +53,113 @@ public function index(TeamsRepository $teamsRepository): Response
         'teamsPOT4' => $teamsPOT4,
         'teamsBarrer' => $teamsBarrer,
 
+        'matchesByGroup' => $matchesByGroup,
+
     ]);
 }
-   
+
+/**
+* @Route("/generate-matches", name="generate_matches")
+*/
+public function generateMatches(TeamsRepository $teamsRepository, EntityManagerInterface $entityManager): Response {
+    
+    // Delete all matches
+    $entityManager->createQuery('DELETE FROM App\Entity\MatchGroup')->execute();
+
+    // Tab all matches
+    $generatedMatches = []; 
+
+    // Create matches for all groups
+    foreach (['POT1', 'POT2', 'POT3', 'POT4'] as $pot) {
+            $teams = $teamsRepository->findBy(['Chapeau' => $pot]);
+            for ($i = 0; $i < count($teams); $i++) {
+                for ($j = $i + 1; $j < count($teams); $j++) {
+                    $match = new MatchGroup();
+                    $match->setTeam1($teams[$i]);
+                    $match->setTeam2($teams[$j]);
+                    $entityManager->persist($match);
+                    // Push to tab
+                    $generatedMatches[] = $match; 
+            }
+        }
+    }
+
+    // Assign score by match
+    foreach ($generatedMatches as $match) {
+        $score = $this->generateScore($match->getTeam1(), $match->getTeam2());
+        $match->setScore($score);
+    }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_index');
+    }
 
 
+/**
+* @Route("/reset-matches", name="reset_matches")
+*/
 
+public function resetMatches(EntityManagerInterface $entityManager): Response {
+        // Delete all matches
+        $entityManager->createQuery('DELETE FROM App\Entity\MatchGroup')->execute();
 
+        return $this->redirectToRoute('app_index');
+    }
+    
+    private function generateScore($teamA, $teamB) {
+        // Prob and logic
+        $random = mt_rand(1, 100);
+        if ($random <= 68) { 
+            $commonScores = ['1-1', '2-0', '1-0', '2-1', '0-0', '0-1', '1-2'];
+            $score = $commonScores[array_rand($commonScores)];
+        } else if ($random <= 84) { 
+            $score = $this->generateHigherButReasonableScore();
+        } else { 
+            $score = $this->generateUncommonScore();
+        }
+    
+        // use ranking function for create a logic score
+        return $this->adjustScoreForFifaRanking($score, $teamA, $teamB);
+    }
+    
 
-
-
-
-
-
-
+    // Generate reasonable score and compare all scores
+    private function generateHigherButReasonableScore() {
+        $scoreA = mt_rand(0, 3);
+        $scoreB = mt_rand(0, 3);
+        if ($scoreA == $scoreB) {
+            $scoreB = ($scoreB + 1) % 4;
+        }
+        return $scoreA . '-' . $scoreB;
+    }
+    
+    private function generateUncommonScore() {
+        $scoreA = mt_rand(0, 4);
+        $scoreB = mt_rand(0, 4);
+        return $scoreA . '-' . $scoreB;
+    }
+    
+    private function adjustScoreForFifaRanking($score, $teamA, $teamB) {
+        list($scoreA, $scoreB) = explode('-', $score);
+    
+        $rankA = $teamA->getRankFifa();
+        $rankB = $teamB->getRankFifa();
+    
+        $rankDifference = abs($rankA - $rankB);
+    
+        if ($rankDifference != 0 && $rankA != $rankB) {
+            if ($rankA < $rankB) {
+                // Team A have best rank so ajust the score for him
+                $scoreA += ($rankDifference >= 10 && $scoreA < 4) ? 1 : 0;
+            } else {
+                // the same for Team B
+                $scoreB += ($rankDifference >= 10 && $scoreB < 4) ? 1 : 0;
+            }
+        }
+    
+        return min($scoreA, 8) . '-' . min($scoreB, 8); 
+    }
 
     
 }
