e166a6eac59f62708054e29815e656cb53c68ab4
thomixlz <thomasbregler@protonmail.com>
2023-12-21 09:43:02 +0100

    add tab by ranking & display group tournament

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,20 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TeamsRepository;
 
-class DefaultController extends AbstractController
-{
+class DefaultController extends AbstractController {
 
     
-  
 /**
-    * @Route("/", name="app_index")
-    */
-    public function index(): Response
-    {
-        return $this->render('index.html.twig');
-    }
+* @Route("/", name="app_index")
+*/
+public function index(TeamsRepository $teamsRepository): Response
+{
 
+    $teams = $teamsRepository->findAll();
+    $teamsByRank = $teamsRepository->findBy([], ['rankFifa' => 'ASC']);
+
+    $teamsPOT1 = $teamsRepository->findBy(['Chapeau' => 'POT1']);
+    $teamsPOT2 = $teamsRepository->findBy(['Chapeau' => 'POT2']);
+    $teamsPOT3 = $teamsRepository->findBy(['Chapeau' => 'POT3']);
+    $teamsPOT4 = $teamsRepository->findBy(['Chapeau' => 'POT4']);
+
+
+
+
+
+    return $this->render('index.html.twig', [
+        'teams' => $teams,
+        'teamsByRank' => $teamsByRank,
+        'teamsPOT1' => $teamsPOT1,
+        'teamsPOT2' => $teamsPOT2,
+        'teamsPOT3' => $teamsPOT3,
+        'teamsPOT4' => $teamsPOT4,
+
+    ]);
+}
    
 
 
