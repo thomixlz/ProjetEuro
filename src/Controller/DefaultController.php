d39dbc81942a9bf88e9285a1452804cf411c5024
thomixlz <thomasbregler@protonmail.com>
2023-12-22 11:14:21 +0100

    add ranking table | Gen and by POT

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -39,6 +39,58 @@ public function index(TeamsRepository $teamsRepository, MatchGroupRepository $ma
     $matchesGroup = $matchGroupRepository->findAll();
     $matchesBarrier = $matchBarrierRepository->findAll();
 
+    $classementGroup = $teamsRepository->findAll();
+    $classementGroupPOT1 = $teamsRepository->findBy(['Chapeau' => 'POT1']);
+    $classementGroupPOT2 = $teamsRepository->findBy(['Chapeau' => 'POT2']);
+    $classementGroupPOT3 = $teamsRepository->findBy(['Chapeau' => 'POT3']);
+    $classementGroupPOT4 = $teamsRepository->findBy(['Chapeau' => 'POT4']);
+
+
+    usort($classementGroup, function($a, $b) {
+        // Sort by points (descending)
+        if ($a->getPoints() !== $b->getPoints()) {
+            return $b->getPoints() - $a->getPoints();
+        }
+        return strcmp($a->getName(), $b->getName());
+    });
+
+    usort($classementGroupPOT1, function($a, $b) {
+        // Sort by points (descending)
+        if ($a->getPoints() !== $b->getPoints()) {
+            return $b->getPoints() - $a->getPoints();
+        }
+        return strcmp($a->getName(), $b->getName());
+    });
+
+
+    usort($classementGroupPOT2, function($a, $b) {
+        // Sort by points (descending)
+        if ($a->getPoints() !== $b->getPoints()) {
+            return $b->getPoints() - $a->getPoints();
+        }
+        return strcmp($a->getName(), $b->getName());
+    });
+
+
+    usort($classementGroupPOT3, function($a, $b) {
+        // Sort by points (descending)
+        if ($a->getPoints() !== $b->getPoints()) {
+            return $b->getPoints() - $a->getPoints();
+        }
+        return strcmp($a->getName(), $b->getName());
+    });
+
+    usort($classementGroupPOT4, function($a, $b) {
+        // Sort by points (descending)
+        if ($a->getPoints() !== $b->getPoints()) {
+            return $b->getPoints() - $a->getPoints();
+        }
+        return strcmp($a->getName(), $b->getName());
+    });
+
+
+
+
 
 
     // Group match by 'Chapeau'
@@ -63,6 +115,8 @@ public function index(TeamsRepository $teamsRepository, MatchGroupRepository $ma
         'teams' => $teams,
         'teamsByRank' => $teamsByRank,
 
+        
+
        
       
         'teamsPOT1' => $teamsPOT1,
@@ -75,6 +129,12 @@ public function index(TeamsRepository $teamsRepository, MatchGroupRepository $ma
         'matchesByBarrier' => $matchesByBarrier,
 
         'barrierQualifier' => $teamsBarrierQualifer,
+
+        'classementTeamGroupPOT1' => $classementGroupPOT1,
+        'classementTeamGroupPOT2' => $classementGroupPOT2,
+        'classementTeamGroupPOT3' => $classementGroupPOT3,
+        'classementTeamGroupPOT4' => $classementGroupPOT4,
+        'classementTeamGroup' => $classementGroup,
 
     ]);
 }
@@ -111,6 +171,42 @@ public function generateMatches(TeamsRepository $teamsRepository, EntityManagerI
         $match->setScore($score);
     }
 
+
+    foreach ($generatedMatches as $match) {
+        // Your existing score setting logic
+    
+        // Add logic to update team stats
+        $team1 = $match->getTeam1();
+        $team2 = $match->getTeam2();
+        list($score1, $score2) = explode('-', $match->getScore());
+    
+        // Update goals
+        $team1->setGoalsScored($team1->getGoalsScored() + $score1);
+        $team2->setGoalsScored($team2->getGoalsScored() + $score2);
+    
+        $team1->setGoalsConceded($team1->getGoalsConceded() + $score2);
+        $team2->setGoalsConceded($team2->getGoalsConceded() + $score1);
+    
+        // Update wins, draws, losses, and points
+        if ($score1 > $score2) { // Team 1 wins
+            $team1->setWins($team1->getWins() + 1);
+            $team1->setPoints($team1->getPoints() + 3);
+            $team2->setLosses($team2->getLosses() + 1);
+        } elseif ($score1 < $score2) { // Team 2 wins
+            $team2->setWins($team2->getWins() + 1);
+            $team2->setPoints($team2->getPoints() + 3);
+            $team1->setLosses($team1->getLosses() + 1);
+        } else { // Draw
+            $team1->setDraws($team1->getDraws() + 1);
+            $team2->setDraws($team2->getDraws() + 1);
+            $team1->setPoints($team1->getPoints() + 1);
+            $team2->setPoints($team2->getPoints() + 1);
+        }
+    
+        $entityManager->persist($team1);
+        $entityManager->persist($team2);
+    }
+
         $entityManager->flush();
 
         return $this->redirectToRoute('app_index');
@@ -123,9 +219,21 @@ public function generateMatches(TeamsRepository $teamsRepository, EntityManagerI
 * @Route("/reset-group-matches", name="reset_group_matches")
 */
 
-public function resetMatches(EntityManagerInterface $entityManager): Response {
+public function resetMatches(TeamsRepository $teamsRepository,EntityManagerInterface $entityManager): Response {
         // Delete all matches
         $entityManager->createQuery('DELETE FROM App\Entity\MatchGroup')->execute();
+
+        $teams = $teamsRepository->findAll();
+foreach ($teams as $team) {
+    $team->setWins(0);
+    $team->setLosses(0);
+    $team->setDraws(0);
+    $team->setGoalsScored(0);
+    $team->setGoalsConceded(0);
+    $team->setPoints(0);
+    $entityManager->persist($team);
+}
+$entityManager->flush();
 
         return $this->redirectToRoute('app_index');
     }
